80fb10f3b9a4041a56fd2f85eb1545bc9a1b3341
Marss616 <67536529+Marss616@users.noreply.github.com>
2024-03-03 21:30:32 +1100

    Update encryption_1.py

--- encryption_1.py
+++ encryption_1.py
@@ -1,10 +1,34 @@
-# this program will use date time as an encryption method, via shift key change.
+# this program will encrypt a message using the Caesar cipher method and a shift key that is generated from the current time
 
 import datetime
 
-# def encrypt(text):
+def create_shift_key_with_date():
+    date = datetime.datetime.now()
+    date_split = str(date).split(" ")
+    shift = 0  
+    
+    for i, v in enumerate(date_split):
+        if i == 1: 
+            for x in v:
+                if x.isdigit():
+                    shift += int(x)
+    return shift % 26
 
+def caesar_cipher(text, shift):
+    encrypted_text = ""
+    
+    for char in text:
+        if char.isalpha():
+            start = 65 if char.isupper() else 97
+            encrypted_char = chr((ord(char) + shift - start) % 26 + start)
+            encrypted_text += encrypted_char
+        else:
+            encrypted_text += char
+            
+    return encrypted_text
 
-date = datetime.datetime.now()
-date_split = str(date).split(" ")
-print(date_split)
+text = "Hello, World!"
+shift = create_shift_key_with_date()
+encrypted_text = caesar_cipher(text, shift)
+print(shift)
+print(encrypted_text)
